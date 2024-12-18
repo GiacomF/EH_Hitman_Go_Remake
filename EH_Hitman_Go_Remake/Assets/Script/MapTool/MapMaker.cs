12374577fffa7922f97c29ddf811c5ac4c8bfe75
GiacomF <giacomofiumana@gmial.com>
2024-12-18 17:27:36 +0100

    Map Maker 1.1
    Implemented Input System, calling action to Place Node

--- EH_Hitman_Go_Remake/Assets/Script/MapTool/MapMaker.cs
+++ EH_Hitman_Go_Remake/Assets/Script/MapTool/MapMaker.cs
@@ -4,31 +4,37 @@ public class MapMaker : MonoBehaviour
 {
     //I'm designing all the script in pseudo-code, and proceed by replacing
 
-    /*
-
-    public Tag ValidTerrain;
     public bool MapEditingMode = false;
+    
+    Camera cam;
+    private void Start()
+    {
+        cam = Camera.main;
+    }
 
-    void Update()
-        if MapEditingMode == true
-            EditNodes()
+    public GameObject MapNodePrefab;
+    public void PlaceNode()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-    private void EditNodes()
-
-        on left click
-            ray check if there is a surface and if the surface is ValidTerrain
-                place a MovementNode on the raycast contact point
+        if(Physics.Raycast(ray, out hit, 100f))
+            {
+                Instantiate(MapNodePrefab, hit.transform.position, Quaternion.identity);
+                /*
                 initialize the MovementNode (defining its parameters and communicating its presence to a Movement/Map Manager)
-                Write a log communicating the Designer a new node was placed
+                Write a log communicating the Designer a new node was placed*/
+            }
 
-            otherwise write a log stating ("Can't place a node here!")
+        //otherwise write a log stating ("Can't place a node here!")
 
+    }
+    /*
         on right click
             check if the ray is hitting a MapNode
                 call DeleteNodeFromMap on the MapNode
 
             otherwise write a log stating ("Couldn't find a node")
-    
     */
 }
 
--- EH_Hitman_Go_Remake/Assets/Script/MapTool/MapManager.cs
+++ EH_Hitman_Go_Remake/Assets/Script/MapTool/MapManager.cs
@@ -8,11 +8,26 @@ public class MapManager : MonoBehaviour
     
     method to make it a Singleton
 
-    Dictionary to match MapNodes and NodePositions
+    Dictionary to match MapNodes and NodePositions NodesToPosDictionary;
     
-    public void AddNodeToMap()
-
+    public void AddNodeToMap(MapNode thisNode, Vector3 nodePosition)
+        add to NodesToPos MapNode and nodePosition
 
     
+    public void RemoveNodeFromMap(Vector3 Position)
+        remove from NodesToPos Positions' value
+
+    public 
+    
+
+    //Draws a grid connecting all the available nodes
+
+    public void ConnectNodes()
+        foreach kvp in NodesToPos 
+            A = take its position 
+            B = take the next kvp's position
+            raycast from A to B to see if there is an obstacle between them
+            make a ray that stretches from A to B
+
     */
 }
