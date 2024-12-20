a016e94e0a0486ba821dff8915cb1e0ba6724467
GiacomF <giacomofiumana@gmial.com>
2024-12-20 13:04:22 +0100

    MapMaker 1.3
    Any object will automatically be mapped by a grid

--- EH_Hitman_Go_Remake/Assets/MapTool/Maker/Map.cs
+++ EH_Hitman_Go_Remake/Assets/MapTool/Maker/Map.cs
@@ -1,50 +1,87 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Map : MonoBehaviour
+//This code will create a functioning Grid upon any Parent Object under which this object is placed
+public class MapGenerator : MonoBehaviour
 {
-    public Dimensions_SO mapDimensions;
-
+    //Struct that synthesizes and makes available all informations from objectToMap
     [Serializable]
-    private struct Node
+    struct objectToMap
     {
-        public Vector2 Coords;
-    }
+        public Vector3 parentSize;
 
-    private struct Grid
-    {
-        public Dictionary<Node, Vector2> NodesToCoords;
-
-        public void Initialize()
+        public objectToMap(Vector3 size)
         {
-            NodesToCoords = new Dictionary<Node, Vector2>();
+            parentSize = size;
+        }
+
+        public Vector3 GetSize()
+        {
+            return parentSize;
         }
     }
 
-    public int Spacing = 1;
-    private Grid myGrid;
-    [SerializeField] int nodesInGrid;
-    private void OnEnable()
+    //Struct that synthesizes and makes available all informations from Grid
+    [Serializable]
+    struct Grid
     {
-        myGrid.Initialize();
+        public Vector2 XY;
+        public Vector3 gridOrigin;
 
-        if(mapDimensions != null)
+        public Grid(Vector2 xy, Vector3 origin)
         {
-            int columns = (int)mapDimensions.Dimensions.x/(Spacing * 2);
-            int rows = (int)mapDimensions.Dimensions.y/(Spacing * 2);
-
-            for(int i = 0; i < columns; i++)
-            {
-                Node newNode = new Node();
-                newNode.Coords = new Vector2(i, 0);
-                myGrid.NodesToCoords.Add(newNode, newNode.Coords);
-            }
-
-            nodesInGrid = myGrid.NodesToCoords.Count;
+            XY = xy;
+            gridOrigin = origin; 
         }
 
-        else
-            Debug.LogWarning("Missing Dimensions!");
+        public Vector3 GetPosition()
+        {
+            return gridOrigin;
+        }
     }
+
+    //Method to Map the objToMap and create a Grid on top
+    objectToMap objToMap;
+    [SerializeField] Grid myGrid;
+    void GenerateGrid()
+    {
+        Transform parentObject = gameObject.transform.parent;
+
+        //Using the Local Scale as measures assures corret position
+        objToMap = new objectToMap(parentObject.localScale);
+
+        //Moving this transform to the gridOrigin for simpler nodes distribution
+        myGrid = new Grid(CalculateDimensions(), FindOriginLocation());
+        transform.position = myGrid.GetPosition();
+
+    }
+
+    void OnEnable()
+    {
+        if(gameObject.transform.parent == null) {Debug.LogWarning("Place MapComponent as MapObject son"); return;}
+        
+        GenerateGrid();    
+    }
+
+    #region GenerateGrid Methods
+    //Calculate the dimensions of the object upon which to draw the map
+    Vector2 CalculateDimensions()
+    {
+        Vector2 dimensionsFound = new Vector2();
+        dimensionsFound.x = objToMap.GetSize().x;
+        dimensionsFound.y = objToMap.GetSize().z;
+
+        return dimensionsFound;
+    }
+
+    //Calculate the origin of the matrix, extrapolated from the origin of parent * Vector3 offset
+    Vector3 FindOriginLocation()
+    {
+        Vector3 size = objToMap.GetSize();
+        Vector3 offset = new Vector3(-size.x / 2, size.y, -size.z / 2);
+        Vector3 gridOrigin = transform.parent.transform.position + offset;
+
+        return gridOrigin;
+    }
+    #endregion
 }
