2731fb2b34cdfd154401dda1e968c5ab6e6dd78b
Federico Paolini <fp@nuxi.it>
2025-01-20 18:56:11 +0100

    Added SFX and audio manager

---
+++ EH_Hitman_Go_Remake/Assets/Sound/AudioManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Singleton")]
+    private static AudioManager _instance;
+    public static AudioManager instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindAnyObjectByType<AudioManager>();
+            if (_instance == null)
+                Debug.LogError("Audio Manager not found, can't create singleton object");
+            return _instance;
+        }
+    }
+
+    public AudioMixer mixer;
+    private AudioSource musicSource;
+    public GameObject SFXSource;
+
+    public AudioClip BGM;
+
+    private void Start()
+    {
+        musicSource = GetComponent<AudioSource>();
+        PlayMusicTrack(BGM);
+    }
+
+    public void PlayMusicTrack(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+
+    public void PlaySoundFX(AudioClip clip, float randomPitch)
+    {
+        AudioSource currentSFX = GameObject.Instantiate(SFXSource).GetComponent<AudioSource>();
+        currentSFX.pitch = Random.Range(1 - randomPitch, 1 + randomPitch);
+        currentSFX.clip = clip;
+        currentSFX.Play();
+        Destroy(currentSFX.gameObject, clip.length);
+    }
+
+    public void SetVolume(string chosenMixer, float newVolume)
+    {
+        mixer.SetFloat(chosenMixer, newVolume);
+    }
+}
